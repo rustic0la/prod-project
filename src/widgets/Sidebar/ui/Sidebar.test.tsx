a62d471c62f627d62210942c2ab8a7333027a524
rustic0la <vm853404@protonmail.com>
2023-03-05 12:20:25 +0300

    add reg-cli

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslations/renderWithTranslations';
+import { componentRender } from 'shared/lib/tests/componentRender/conponentRender';
 import { Sidebar } from './Sidebar';
 
 describe('testing button', () => {
   test('with only parameter', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('Sidebar')).toBeInTheDocument();
   });
 
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleButton = screen.getByTestId('sidebar-toggle');
     fireEvent.click(toggleButton);
     expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
