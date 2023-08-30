557b80e493f7ee66b539d72af5c4e7f80145fd16
rustic0la <vm853404@protonmail.com>
2023-08-30 14:50:04 +0300

    add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,28 +1,10 @@
 import type webpack from 'webpack';
 import { type BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-  const babelLoader = {
-    test: /\.m?(js|jsx|ts|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-        plugins: [
-          [
-            'i18next-extract',
-            {
-              nsSeparator: '~',
-              locales: ['ru', 'en'],
-              keyAsDefaultValue: true,
-            },
-          ],
-        ],
-      },
-    },
-  };
+  const babelLoader = buildBabelLoader(options.isDev);
 
   const svgLoader = {
     test: /\.svg$/,
---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,23 @@
+export function buildBabelLoader(isDev: boolean) {
+  return {
+    test: /\.m?(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            {
+              nsSeparator: '~',
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true,
+            },
+          ],
+          isDev && require.resolve('react-refresh/babel'),
+        ].filter(Boolean),
+      },
+    },
+  };
+}
